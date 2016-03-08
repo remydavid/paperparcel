e576d1641dc5f148d5ac044d35216ed707c18219
Rémy DAVID <remy@redshift.fr>
2016-03-08 10:03:10 +0100

    removed android.R

--- kotlin-example/src/main/kotlin/nz/bradcampbell/paperparcel/kotlinexample/MainActivity.kt
+++ kotlin-example/src/main/kotlin/nz/bradcampbell/paperparcel/kotlinexample/MainActivity.kt
@@ -1,10 +1,10 @@
 package nz.bradcampbell.paperparcel.kotlinexample
 
-import android.R
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 import nz.bradcampbell.paperparcel.PaperParcels
 import nz.bradcampbell.paperparcel.TypedParcelable
 import java.text.SimpleDateFormat
